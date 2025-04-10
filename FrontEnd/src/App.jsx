803f744e55b42d3079e541b20431767a306b3cf6
PichPong <pichpong01@gmail.com>
2025-04-10 14:18:28 +0700

    login

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RouterComponent from './router';
+
+function App() {
+
+  return (
+    <RouterComponent/>
+  );
+
+}
+
+export default App
